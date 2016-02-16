dab7edf6c39ec4a5f6d129a9b46f3c795acc969e
Tom <caigertom@gmail.com>
2016-02-17 11:02:39 +1300

    renamed root file

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,15 +3,10 @@
 global $project;
 $project = 'mysite';
 
-global $databaseConfig;
-$databaseConfig = array(
-	'type' => 'MySQLDatabase',
-	'server' => 'localhost',
-	'username' => 'msupplyn_admin',
-	'password' => 'admin1234',
-	'database' => 'msupplyn_SS_msupply',
-	'path' => ''
-);
+global $database;
+$database = 'SS_msupply';
+
+require_once('conf/ConfigureFromEnv.php');
 
 // Set the site locale
 i18n::set_locale('en_US');
